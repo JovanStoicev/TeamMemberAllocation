914519ab737778217b5d07fb1f8d8166b871b9d1
JovanStoicev <67899512+JovanStoicev@users.noreply.github.com>
2023-03-16 11:01:47 +0200

    Removing cpr symbol :D

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,6 @@ const Footer = () => {
       <div className="row justify-content-center mt-3 mb-4">
         <div className="col-8">
           <h5>Team Member Allocation app - March 2023</h5>
-          <p>Copyright &copy; - Jovan Stoicev</p>
         </div>
       </div>
     </footer>
