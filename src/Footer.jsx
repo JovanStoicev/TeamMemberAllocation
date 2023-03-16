ef74ef9d66df9aca5fe14b65f1f17bdf0c0a016c
Jovan Stoicev <67899512+JovanStoicev@users.noreply.github.com>
2023-03-16 09:00:49 +0000

    Added footer color and copyright

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,11 @@ const Footer = () => {
   var today = new Date();
 
   return (
-    <footer className="container footer-style/" >
+    <footer className="container footer-style" >
       <div className="row justify-content-center mt-3 mb-4">
         <div className="col-8">
-          <h5>Team Member Allocation app - {today.getDate()}/{today.getMonth() + 1}/{today.getFullYear()}</h5>
+          <h5>Team Member Allocation app - March 2023</h5>
+          <p>Copyright &copy; - Jovan Stoicev</p>
         </div>
       </div>
     </footer>
